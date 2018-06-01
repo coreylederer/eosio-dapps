8d137c532bda1476ced69b623b7df7bda1f5ee18
joneric <mrjec@protonmail.com>
2018-06-01 12:46:58 -0700

    laying out new architecture

--- arbitration/arbitration.cpp
+++ arbitration/arbitration.cpp
@@ -2,156 +2,150 @@
  *  @file
  *  @copyright defined in eos/LICENSE.txt
  */
+#include <eosiolib/singleton.hpp>
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
-#include <eosiolib/singleton.hpp>
-#include <string>
-#include <algorithm>
 #include <vector>
+#include <string>
 
+using eosio::const_mem_fun;
+using eosio::indexed_by;
 using eosio::asset;
-using eosio::print;
+using eosio::vector;
 using std::string;
 
 class arbitration : public eosio::contract {
     public:
         explicit arbitration(action_name self) : contract(self) {}
 
-        struct document {
-            string description;
-            string link;
-            checksum256 hash_of_contents;
-        };
-
-        struct filing {
-            account_name belongs_to;
-            std::vector<document> documents;
-            std::vector<transaction_id_type> tx_ids;
-        };
-
-        struct user {
-
-        }
-
-        //@abi action
-        void submitclaim(const account_name claimant, const std::vector<account_name>& respondents,
-                         const std::vector<string>& tx_ids, const std::vector<document>& documents,
-                         const asset& fee) {
-            require_auth(claimant);
-
-            claim_index claims(_self,_self);
-
-            uint64_t claim_id;
-            claims.emplace(claimant, [&](auto& claim) {
-                claim.id = next_claim_id();
-                claim_id = claim.id;
-                claim.claimant = claimant;
-                claim.respondents = respondents;
-                claim.tx_ids = tx_ids;
-                claim.documents = documents;
-                claim.fee = fee;
-                claim.fee_paid = true;
-            });
-            print("Your claim id is ", claim_id, ".");
-        }
-
-        //@abi action
-        void opencase(const uint64_t claim_id) {
-            require_auth(_self);
-
-            claim_index claims(_self, _self);
-            auto ctoac = claims.get(claim_id); // claim to open as case
-
-            eosio_assert(ctoac.fee_paid, "Cannot open a case if the filing fee has not been paid.");
-            eosio_assert(ctoac.bond_fronted, "Cannot open a case if the required bond has not been fronted.");
-            eosio_assert(!ctoac.is_rejected, "Cannot open a case for a claim that was rejected.");
-            eosio_assert(!ctoac.claim_dropped, "Cannot open a case for a claim that was dropped.");
-
-            uint64_t case_id;
-            arbcase_index arbcases(_self, _self);
-            arbcases.emplace(_self, [&](auto& arbcase) {
-                arbcase.id              = next_case_id();
-                case_id                 = arbcase.id;
-                arbcase.claimants.push_back(ctoac.claimant);
-                for (auto &respondent : ctoac.respondents) {
-                    filing respondent_filing;
-                    respondent_filing.belongs_to = respondent;
-                    arbcase.filings.push_back(respondent_filing);
-                    arbcase.respondents.push_back(respondent);
-                }
-                filing claimant_filing{ctoac.claimant,ctoac.documents,ctoac.tx_ids};
-                arbcase.filings.push_back(claimant_filing);
-                arbcase.time_opened = now();
-            });
-
-            auto claims_itr = claims.find(claim_id);
-
-            claims.modify( claims_itr, 0, [&]( auto& claim ) {
-                claim.can_delete = true;
-            });
-            
-            print("The case id is ", case_id, ".");
-        }
-
     private:
         //@abi table claim i64
         struct claim {
             uint64_t id;
-            account_name claimant;
-            std::vector<account_name> respondents;
-            std::vector<string> tx_ids;
-            std::vector<document> documents;
-            bool claim_dropped = false;
-            bool can_delete = false;
-            bool is_rejected = false;
-            document rejection_reason;
-            asset fee;
-            bool fee_paid = false;
-            asset bond;
-            bool bond_fronted = false;
-
             uint64_t primary_key() const { return id; }
-
-            EOSLIB_SERIALIZE( claim, (id)(claimant)(respondents)(tx_ids)(documents)(claim_dropped)
-                                     (can_delete)(is_rejected)(rejection_reason)(fee)(fee_paid)(bond)
-                                     (bond_fronted) )
+            EOSLIB_SERIALIZE( claim, (id) )
         };
         typedef eosio::multi_index< N(claim), claim > claim_index;
 
         //@abi table arbcase i64
         struct arbcase {
             uint64_t id;
-            std::vector<account_name> claimants;
-            std::vector<account_name> respondents;
-            std::vector<account_name> arbitrators;
-            std::vector<filing> filings;
-            time time_opened;
-            bool case_dropped = false;
-            bool can_delete = false;
-            bool is_resolved = false;
-            time time_closed;
-            asset fee;
-            bool fee_paid = false;
-            asset bond;
-            bool bond_fronted = false;
-            bool bond_dispersed = false;
-            document ruling;
-            document remedy;
-            bool requested_remedy = false;
-            bool remedy_fulfilled = false;
-
             uint64_t primary_key() const { return id; }
-
-            EOSLIB_SERIALIZE( arbcase, (id)(claimants)(respondents)(arbitrators)(filings)
-                            (time_opened)(case_dropped)(can_delete)(is_resolved)(time_closed)(fee)
-                            (fee_paid)(bond)(bond_fronted)(bond_dispersed)(ruling)(remedy)
-                            (requested_remedy)(remedy_fulfilled) )
+            EOSLIB_SERIALIZE( arbcase, (id) )
         };
         typedef eosio::multi_index< N(arbcase), arbcase > arbcase_index;
 
+        //@abi table claimant i64
+        struct claimant {
+            account_name id;
+            account_name primary_key() const { return id; }
+            EOSLIB_SERIALIZE( claimant, (id) )
+        };
+        typedef eosio::multi_index< N(claimant), claimant > claimant_index;
+
+        //@abi table respondent i64
+        struct respondent {
+            account_name id;
+            account_name primary_key() const { return id; }
+            EOSLIB_SERIALIZE( respondent, (id) )
+        };
+        typedef eosio::multi_index< N(respondent), respondent > respondent_index;
+
+        //@abi table arbitrator i64
+        struct arbitrator {
+            account_name id;
+            account_name primary_key() const { return id; }
+            EOSLIB_SERIALIZE( arbitrator, (id) )
+        };
+        typedef eosio::multi_index< N(arbitrator), arbitrator > arbitrator_index;
+
+        //@abi table document i64
+        struct document {
+            uint64_t id;
+            account_name owner;
+            string description;
+            string link;
+            checksum256 hash_of_contents;
+            uint64_t primary_key() const { return id; }
+            account_name by_owner() const { return owner; }
+            EOSLIB_SERIALIZE( document, (id)(owner)(description)(link)(hash_of_contents) )
+        };
+        typedef eosio::multi_index< N(document), document,
+            indexed_by< N(byowner), const_mem_fun<document, account_name, &document::by_owner> >
+            > document_index;
+
+        //@abi table transaction i64
+        struct transaction {
+            uint64_t id;
+            account_name owner;
+            string description;
+            string link;
+            string tx_id;
+            uint64_t primary_key() const { return id; }
+            account_name by_owner() const { return owner; }
+            EOSLIB_SERIALIZE( transaction, (id)(owner)(description)(link)(tx_id) )
+        };
+        typedef eosio::multi_index< N(transaction), transaction,
+            indexed_by< N(byowner), const_mem_fun<transaction, account_name, &transaction::by_owner> >
+            > transaction_index;
+
+        //@abi table bondpayment i64
+        struct bondpayment {
+            uint64_t id;
+            account_name owner;
+            asset amount;
+            uint64_t primary_key() const { return id; }
+            account_name by_owner() const { return owner; }
+            EOSLIB_SERIALIZE( transaction, (id)(owner)(amount) )
+        };
+        typedef eosio::multi_index< N(bondpayment), bondpayment,
+            indexed_by< N(byowner), const_mem_fun<bondpayment, account_name, &bondpayment::by_owner> >
+            > bondpayment_index;
+
+        //@abi table bondowed i64
+        struct bondowed {
+            account_name id;
+            asset amount;
+            bool paid = false;
+            account_name primary_key() const { return id; }
+            EOSLIB_SERIALIZE( transaction, (id)(amount)(paid) )
+        };
+        typedef eosio::multi_index< N(bondowed), bondowed > bondowed_index;
+
+        //@abi table arbcasefee i64
+        struct arbcasefee {
+            uint64_t id;
+            asset fee;
+            bool current = false;
+            uint64_t primary_key() const { return id; }
+            bool by_current() const { return current; }
+            EOSLIB_SERIALIZE( transaction, (id)(fee)(bond) )
+        };
+        typedef eosio::multi_index< N(arbcasefee), arbcasefee,
+            indexed_by< N(byowner), const_mem_fun<arbcasefee, bool, &arbcasefee::by_current> >
+            > arbcasefee_index;
+
+        //@abi table arbcasebond i64
+        struct arbcasebond {
+            uint64_t id;
+            asset bond;
+            bool current = false;
+            uint64_t primary_key() const { return id; }
+            bool by_current() const { return current; }
+            EOSLIB_SERIALIZE( transaction, (id)(fee)(bond) )
+        };
+        typedef eosio::multi_index< N(arbcasebond), arbcasebond,
+            indexed_by< N(byowner), const_mem_fun<arbcasebond, bool, &arbcasebond::by_current> >
+            > arbcasebond_index;
+
         typedef uint64_t id;
         typedef eosio::singleton<N(claimid), id>  claim_id_index;
-        typedef eosio::singleton<N(caseid), id>  case_id_index;
+        typedef eosio::singleton<N(arbcaseid), id>  arbcase_id_index;
+        typedef eosio::singleton<N(documentid), id>  document_id_index;
+        typedef eosio::singleton<N(transactionid), id>  transaction_id_index;
+        typedef eosio::singleton<N(bondpaymentid), id>  bondpayment_id_index;
+        typedef eosio::singleton<N(arbcasefeeid), id>  arbcasefee_id_index;
+        typedef eosio::singleton<N(arbcasebondid), id>  arbcasebond_id_index;
 
         id next_claim_id(){
             claim_id_index last_claim_id(_self, _self);
@@ -160,12 +154,47 @@ class arbitration : public eosio::contract {
             return lclaimid;
         }
 
-        id next_case_id(){
-            case_id_index last_case_id(_self, _self);
-            id lcaseid = last_case_id.exists() ? last_case_id.get() + 1 : 0;
-            last_case_id.set(lcaseid,_self);
-            return lcaseid;
+        id next_arbcase_id(){
+            arbcase_id_index last_arbcase_id(_self, _self);
+            id larbcaseid = last_arbcase_id.exists() ? last_arbcase_id.get() + 1 : 0;
+            last_arbcase_id.set(larbcaseid,_self);
+            return larbcaseid;
+        }
+
+        id next_document_id(){
+            document_id_index last_document_id(_self, _self);
+            id ldocumentid = last_document_id.exists() ? last_document_id.get() + 1 : 0;
+            last_document_id.set(ldocumentid,_self);
+            return ldocumentid;
+        }
+
+        id next_transaction_id(){
+            transaction_id_index last_transaction_id(_self, _self);
+            id ltransactionid = last_transaction_id.exists() ? last_transaction_id.get() + 1 : 0;
+            last_transaction_id.set(ltransactionid,_self);
+            return ltransactionid;
+        }
+
+        id next_bondpayment_id(){
+            bondpayment_id_index last_bondpayment_id(_self, _self);
+            id lbondpaymentid = last_bondpayment_id.exists() ? last_bondpayment_id.get() + 1 : 0;
+            last_bondpayment_id.set(lbondpaymentid,_self);
+            return lbondpaymentid;
+        }
+
+        id next_arbcasefee_id(){
+            arbcasefee_id_index last_arbcasefee_id(_self, _self);
+            id larbcasefeeid = last_arbcasefee_id.exists() ? last_arbcasefee_id.get() + 1 : 0;
+            last_arbcasefee_id.set(larbcasefeeid,_self);
+            return larbcasefeeid;
+        }
+
+        id next_arbcasebond_id(){
+            arbcasebond_id_index last_arbcasebond_id(_self, _self);
+            id larbcasebondid = last_arbcasebond_id.exists() ? last_arbcasebond_id.get() + 1 : 0;
+            last_arbcasebond_id.set(larbcasebondid,_self);
+            return larbcasebondid;
         }
 };
 
-EOSIO_ABI( arbitration, (submitclaim)(opencase) )
+EOSIO_ABI( arbitration,  )
