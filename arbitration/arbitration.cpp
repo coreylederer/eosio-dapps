04bafa7dd440bb46662cec3aa4a9a74e9450e324
joneric <mrjec@protonmail.com>
2018-06-04 19:09:07 -0700

    worked on supporting functions

--- arbitration/arbitration.cpp
+++ arbitration/arbitration.cpp
@@ -25,47 +25,115 @@ class arbitration : public eosio::contract {
     public:
         explicit arbitration(action_name self) : contract(self) {}
 
+        bool exists(controls::Entity what, const uint64_t entity_id) {
+            switch(what) {
+                case controls::arbcase: {
+                    arbcase_index arbcases(_self, _self);
+                    auto arbcases_itr = arbcases.find(entity_id);
+                    return arbcases_itr != arbcases.end();
+                }
+                case controls::claim: {
+                    claim_index claims(_self, _self);
+                    auto claims_itr = claims.find(entity_id);
+                    return claims_itr != claims.end();
+                }
+                default: {
+                    return false;
+                }
+            }
+        }
+
         bool exists(controls::Person who, controls::Entity where, const uint64_t entity_id,
                     const account_name person) {
             switch(who) {
-                case controls::arbitrator:
+                case controls::arbitrator: {
                     switch(where) {
-                        case controls::arbcase:
-                        case controls::claim:
+                        case controls::arbcase: {
+                            arbitrator_index arbitrators(_self, entity_id);
+                            auto arbitrators_itr = arbitrators.find(person);
+                            return arbitrators_itr != arbitrators.end();
+                        }
+                        default: {
+                            return false;
+                        }
                     }
-                case controls::claimant:
+                }
+                case controls::claimant: {
                     switch(where) {
-                        case controls::arbcase:
-                        case controls::claim:
+                        case controls::arbcase: {
+                            claimant_index claimants(_self, entity_id); // entity_id is an arbcase id being used as scope
+                            auto claimants_itr = claimants.find(person);
+                            return claimants_itr != claimants.end();
+                        }
+                        case controls::claim: {
+                            claimant_index claimants(_self, entity_id); // entity_id is a claim id being used as scope
+                            auto claimants_itr = claimants.find(person);
+                            return claimants_itr != claimants.end();
+                        }
+                        default: {
+                            return false;
+                        }
                     }
-                case controls::respondent:
+                }
+                case controls::respondent: {
                     switch(where) {
-                        case controls::arbcase:
-                        case controls::claim:
+                        case controls::arbcase: {
+                            respondent_index respondents(_self, entity_id); // entity_id is an arbcase id being used as scope
+                            auto respondents_itr = respondents.find(person);
+                            return respondents_itr != respondents.end();
+                        }
+                        case controls::claim: {
+                            respondent_index respondents(_self, entity_id); // entity_id is a claim id being used as scope
+                            auto respondents_itr = respondents.find(person);
+                            return respondents_itr != respondents.end();
+                        }
+                        default: {
+                            return false;
+                        }
                     }
+                }
+                default: {
+                    return false;
+                }
             }
         }
 
         bool exists(controls::Item what, controls::Entity where, const uint64_t entity_id,
                     const uint64_t item_id) {
             switch(what) {
-                case controls::document:
+                case controls::document: {
                     switch(where) {
-                        case controls::arbcase:
-                        case controls::claim:
+                        case controls::arbcase: {
+                            document_index documents(_self, entity_id); // entity_id is an arbcase id being used as scope
+                            auto documents_itr = documents.find(item_id);
+                            return documents_itr != documents.end();
+                        }
+                        case controls::claim: {
+                            document_index documents(_self, entity_id); // entity_id is a claim id being used as scope
+                            auto documents_itr = documents.find(item_id);
+                            return documents_itr != documents.end();
+                        }
+                        default: {
+                            return false;
+                        }
                     }
-                case controls::transaction:
+                }
+                case controls::transaction: {
                     switch(where) {
-                        case controls::arbcase:
-                        case controls::claim:
-                    }
-            }
-        }
+                        case controls::arbcase: {
 
-        bool exists(controls::Entity what, const uint64_t entity_id) {
-            switch(what) {
-                case controls::arbcase:
-                case controls::claim:
+                        }
+                        case controls::claim: {
+
+                        }
+                        default: {
+                            return false;
+                        }
+                    }
+                }
+                default: {
+                    return false;
+                }
             }
         }
 
@@ -73,8 +141,11 @@ class arbitration : public eosio::contract {
                  const uint64_t arbcase_id, const account_name authority) {
             switch(who) {
                 case controls::arbitrator:
+                    break;
                 case controls::claimant:
+                    break;
                 case controls::respondent:
+                    break;
             }
         }
 
@@ -82,8 +153,11 @@ class arbitration : public eosio::contract {
                     const uint64_t arbcase_id, const account_name authority) {
             switch(who) {
                 case controls::arbitrator:
+                    break;
                 case controls::claimant:
+                    break;
                 case controls::respondent:
+                    break;
             }
         }
 
@@ -94,13 +168,19 @@ class arbitration : public eosio::contract {
                 case controls::document:
                     switch(where) {
                         case controls::arbcase:
+                            break;
                         case controls::claim:
+                            break;
                     }
+                    break;
                 case controls::transaction:
                     switch(where) {
                         case controls::arbcase:
+                            break;
                         case controls::claim:
+                            break;
                     }
+                    break;
             }
         }
 
@@ -111,13 +191,19 @@ class arbitration : public eosio::contract {
                 case controls::document:
                     switch(where) {
                         case controls::arbcase:
+                            break;
                         case controls::claim:
+                            break;
                     }
+                    break;
                 case controls::transaction:
                     switch(where) {
                         case controls::arbcase:
+                            break;
                         case controls::claim:
+                            break;
                     }
+                    break;
             }
         }
 
@@ -131,7 +217,9 @@ class arbitration : public eosio::contract {
         void close(controls::Entity what, const account_name authority, uint64_t entity_id) {
             switch(what) {
                 case controls::arbcase:
+                    break;
                 case controls::claim:
+                    break;
             }
         }
 
@@ -139,9 +227,94 @@ class arbitration : public eosio::contract {
                  const uint64_t entity_id = 0) {
             switch(what) {
                 case controls::fee:
+                    break;
                 case controls::bond:
+                    break;
                 case controls::submittalfee:
+                    break;
             }
+        }
+
+        void pay(controls::Monies what, asset amount, const account_name who,
+                 const uint64_t entity_id = 0) {
+            switch(what) {
+                case controls::fee:
+                    break;
+                case controls::bond:
+                    break;
+                case controls::submittalfee:
+                    break;
+            }
+        }
+
+        //@abi action
+        void opencase() {
+
+        }
+
+        //@abi action
+        void closecase() {
+
+        }
+
+        //@abi action
+        void openclaim() {
+
+        }
+
+        //@abi action
+        void closeclaim() {
+
+        }
+
+        //@abi action
+        void adddoc() {
+
+        }
+
+        //@abi action
+        void removedoc() {
+
+        }
+
+        //@abi action
+        void addtx() {
+
+        }
+
+        //@abi action
+        void removetx() {
+
+        }
+
+        //@abi action
+        void addarb() {
+
+        }
+
+        //@abi action
+        void removearb() {
+
+        }
+
+        //@abi action
+        void addclmnt() {
+
+        }
+
+        //@abi action
+        void removeclmnt() {
+
+        }
+
+        //@abi action
+        void addresp() {
+
+        }
+
+        //@abi action
+        void removeresp() {
+
         }
 
     private:
@@ -273,26 +446,18 @@ class arbitration : public eosio::contract {
         typedef eosio::singleton< N(submittalfee), submittalfee > submittalfee_index;
 
         typedef uint64_t id;
-        typedef eosio::singleton<N(claimid), id>  claim_id_index;
-        typedef eosio::singleton<N(arbcaseid), id>  arbcase_id_index;
+        typedef eosio::singleton<N(entityid), id>  entity_id_index;
         typedef eosio::singleton<N(documentid), id>  document_id_index;
         typedef eosio::singleton<N(transactionid), id>  transaction_id_index;
         typedef eosio::singleton<N(bondpaymentid), id>  bondpayment_id_index;
         typedef eosio::singleton<N(arbcasefeeid), id>  arbcasefee_id_index;
         typedef eosio::singleton<N(arbcasebondid), id>  arbcasebond_id_index;
 
-        id next_claim_id(){
-            claim_id_index last_claim_id(_self, _self);
-            id lclaimid = last_claim_id.exists() ? last_claim_id.get() + 1 : 0;
-            last_claim_id.set(lclaimid,_self);
-            return lclaimid;
-        }
-
-        id next_arbcase_id(){
-            arbcase_id_index last_arbcase_id(_self, _self);
-            id larbcaseid = last_arbcase_id.exists() ? last_arbcase_id.get() + 1 : 0;
-            last_arbcase_id.set(larbcaseid,_self);
-            return larbcaseid;
+        id next_entity_id(){
+            entity_id_index last_entity_id(_self, _self);
+            id lentityid = last_entity_id.exists() ? last_entity_id.get() + 1 : 0;
+            last_entity_id.set(lentityid,_self);
+            return lentityid;
         }
 
         id next_document_id(){
