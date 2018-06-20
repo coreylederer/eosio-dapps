50053d17b05f3c8f71dc8707e2516bc1778964d4
joneric <mrjec@protonmail.com>
2018-06-20 10:48:02 -0700

    it compiles and sets

---
+++ arbitration/build/CMakeFiles/wast.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/wast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
